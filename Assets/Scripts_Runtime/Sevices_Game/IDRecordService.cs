5db2a11588277fbc366f4ec943d3483b213d3a6b
onovich <hjn1110@163.com>
2024-04-21 23:09:09 +0800

    <misc> remove: 冗余代码

--- Assets/Scripts_Runtime/Sevices_Game/IDRecordService.cs
+++ Assets/Scripts_Runtime/Sevices_Game/IDRecordService.cs
@@ -1,17 +1,7 @@
 namespace Alter {
 
     public class IDRecordService {
-
-        int roleEntityID;
-
-        public IDRecordService() { }
-
-        public int PickRoleEntityID() {
-            return ++roleEntityID;
-        }
-
         public void Reset() {
-            roleEntityID = 0;
         }
     }
 
