7167fde93ff4a1ce6e0ef1d8de7c38a0beb4deb4
onovich <hjn1110@163.com>
2024-05-13 22:58:11 +0800

    <refactor> modify: 回退 BakeShape 方案

--- Assets/Scripts_Modifier/BlockShapeEM.cs
+++ Assets/Scripts_Modifier/BlockShapeEM.cs
@@ -29,7 +29,7 @@ namespace Alter.Modifier {
             List<Vector2Int> cellList = new List<Vector2Int>();
             for (int x = 0; x < sizeInt.x; x++) {
                 for (int y = 0; y < sizeInt.y; y++) {
-                    if (cells[x, y] > 0) {
+                    if (cells[x, y]) {
                         cellList.Add(new Vector2Int(x, sizeInt.y - 1 - y));
                     }
                 }
@@ -46,22 +46,22 @@ namespace Alter.Modifier {
         }
 
         void GetCells() {
-            cells = new int[sizeInt.x, sizeInt.y];
+            cells = new bool[sizeInt.x, sizeInt.y];
             for (int i = 0; i < shapeTM.cells.Length; i++) {
                 var x = shapeTM.cells[i].x;
                 var y = sizeInt.y - 1 - shapeTM.cells[i].y;
-                cells[x, y] = i + 1;
+                cells[x, y] = true;
             }
         }
 
         [Button("Clear")]
         void Clear() {
-            cells = new int[sizeInt.x, sizeInt.y];
+            cells = new bool[sizeInt.x, sizeInt.y];
         }
 
         [Button("Resize")]
         void Resize() {
-            var newCells = new int[sizeInt.x, sizeInt.y];
+            var newCells = new bool[sizeInt.x, sizeInt.y];
             for (int x = 0; x < Mathf.Min(sizeInt.x, cells.GetLength(0)); x++) {
                 for (int y = 0; y < Mathf.Min(sizeInt.y, cells.GetLength(1)); y++) {
                     newCells[x, y] = cells[x, y];
@@ -71,50 +71,20 @@ namespace Alter.Modifier {
         }
 
         [TableMatrix(DrawElementMethod = "DrawCell", SquareCells = true)]
-        public int[,] cells;
-        int DrawCell(Rect rect, int value) {
-            Event e = Event.current;
-            if (e.type == EventType.MouseDown && rect.Contains(e.mousePosition)) {
-                if (e.button == 0) {  // 鼠标左键
-                    value = FixNextIndex(value + 1);
-                    value = Math.Min(value, shapeTM.cells.Length);
-                } else if (e.button == 1) {  // 鼠标右键
-                    value = FixLastIndex(value - 1);
-                    value = Math.Max(0, value);
-                }
+        public bool[,] cells;
+
+        bool DrawCell(Rect rect, bool value) {
+            if (Event.current.type == EventType.MouseDown &&
+            rect.Contains(Event.current.mousePosition)) {
+                value = !value;
                 GUI.changed = true;
-                e.Use();
+                Event.current.Use();
             }
             EditorGUI.DrawRect(
                 rect.Padding(1),
-                value > 0 ? UnityEngine.Color.grey :
-                UnityEngine.Color.black);
-            EditorGUI.LabelField(rect, value.ToString());
+                value ? new UnityEngine.Color(0.1f, 0.8f, 0.2f) :
+                new UnityEngine.Color(0, 0, 0, 0.5f));
             return value;
-        }
-
-        int FixNextIndex(int value) {
-            if (value == 0) return 0;
-            for (int i = 0; i < cells.GetLength(0); i++) {
-                for (int j = 0; j < cells.GetLength(1); j++) {
-                    if (cells[i, j] == value) {
-                        return ++value;
-                    }
-                }
-            }
-            return 0;
-        }
-
-        int FixLastIndex(int value) {
-            if (value == 0) return 0;
-            for (int i = 0; i < cells.GetLength(0); i++) {
-                for (int j = 0; j < cells.GetLength(1); j++) {
-                    if (cells[i, j] == value) {
-                        return --value;
-                    }
-                }
-            }
-            return 0;
         }
     }
 
